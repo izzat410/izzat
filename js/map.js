11197eff1a65db3efbb816b7b516e4a5ef27cfd3
izzat410 <izzatsyameer99@gmail.com>
2025-06-17 17:46:46 +0800

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,4 @@
+var map = L.map('map').setView([3.139, 101.6869], 13);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
